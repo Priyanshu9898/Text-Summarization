30a7f995ed374264404eaa1c2d3e14ac7237d869
Priyanshu9898 <priyanshumalaviya9210@gmail.com>
2023-08-18 11:25:50 +0530

    frontend done

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from "react";
 import ThemeButton from "./ThemeButton";
+import Link from "next/link";
 
 const Navbar: FC = () => {
   return (
     <div className="flex w-full h-[64px] text-gray-50 items-center justify-between p-4 bg-gray-900">
-      <div className="text-xl md:text-2xl lg:text-2xl">TextSpark</div>
+      <Link href="/">
+        <div className="text-xl md:text-2xl lg:text-2xl">TextSpark</div>
+      </Link>
       <div>
         <ThemeButton />
       </div>
