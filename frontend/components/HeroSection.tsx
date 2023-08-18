2d22ebe8050a149d77c9ce2a621f9c8f9d0f6309
Priyanshu9898 <priyanshumalaviya9210@gmail.com>
2023-08-18 11:01:50 +0530

    animation added in herosection

--- frontend/components/HeroSection.tsx
+++ frontend/components/HeroSection.tsx
@@ -1,12 +1,13 @@
-import React, {FC} from 'react'
+import React, { FC } from 'react';
 
 const HeroSection: FC = () => {
   return (
-    <div className='p-4 md:p-0'>
-        <h1 className='text-3xl font-bold md:text-3xl lg:text-5xl text-center'>Text Summarization by TextSpark</h1>
-        <p className='text-xl font-bold md:text-2xl text-center text-gray-500 dark:text-gray-200 mt-6 flex-wrap'>Instantly transform dense text into clear summaries, saving you time while ensuring you grasp the core concepts.</p>
+    <div className="p-4 md:p-0 opacity-0 animate-fadeIn">
+        <h1 className="typewriter-effect text-3xl font-bold md:text-3xl lg:text-5xl text-center transform translate-y-4 animate-slideUp mb-2">Text Summarization by TextSpark</h1>
+        <p className="text-xl md:text-2xl text-center text-gray-500 dark:text-gray-400 mt-6 flex-wrap transform -translate-y-4 animate-slideDown">Instantly transform dense text into clear summaries, saving you time while ensuring you grasp the core concepts.</p>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
+
