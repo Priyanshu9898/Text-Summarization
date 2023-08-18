b02182c01a5ea0025aa8f84a39bb7fd17ec144d7
Priyanshu9898 <priyanshumalaviya9210@gmail.com>
2023-08-18 10:47:20 +0530

    upload text file button added

--- frontend/components/FormInput.tsx
+++ frontend/components/FormInput.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import UploadButton from "./UploadButton";
 
 const FormInput = () => {
   return (
@@ -16,14 +17,17 @@ const FormInput = () => {
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 resize-y scrollbar-thin scrollbar-thumb-[#718096] scrollbar-track-[#CBD5E0]"
           placeholder="Type or paste your text here..."
         ></textarea>
-        <div className="mt-6">
+        <div className="mt-6 flex  items-center justify-start">
           <button
             type="button"
             className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
           >
             Summarize
           </button>
+        
 
+          <UploadButton />
+      
           <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Reset</button>
         </div>
       </div>
---
+++ frontend/components/UploadButton.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React, { FC, useState, useRef } from "react";
+
+const UploadButton: FC = () => {
+  const [fileContent, setFileContent] = useState<string>("");
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target?.result as string;
+        setFileContent(content);
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={handleButtonClick}
+      className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg bg-white hover:bg-gray-100 border border-gray-200  text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6  mr-2 -ml-1"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      Upload File
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        style={{ display: "none" }}
+        className="hidden"
+        accept=".txt"
+      />
+    </button>
+  );
+};
+
+export default UploadButton;
